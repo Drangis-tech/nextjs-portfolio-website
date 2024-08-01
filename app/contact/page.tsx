39e68cdc00adb30971872661a8b8602bd2a0b6a1
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-01 20:19:26 +0300

    a

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,23 +5,18 @@ import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
 const socials = [
-	{
-		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
-		label: "Twitter",
-		handle: "@chronark_",
-	},
+
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
-		label: "Email",
-		handle: "dev@chronark.com",
+		href: "mailto:nojus@pavadinimas.lt",
+		label: "Elektroninis paštas",
+		handle: "nojus@pavadinimas.lt",
 	},
 	{
 		icon: <Github size={20} />,
 		href: "https://github.com/chronark",
-		label: "Github",
-		handle: "chronark",
+		label: "LinkedIn",
+		handle: "@pavadinimas.agency",
 	},
 ];
 
