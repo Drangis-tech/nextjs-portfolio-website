e96a4094cc7a5502870c37ec1bb526d57e2c2277
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-02 11:30:56 +0300

    a

--- app/apie-mus/page.tsx
+++ app/apie-mus/page.tsx
@@ -24,15 +24,7 @@ export default function AboutUs() {
     <div className="relative flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
       <nav className="my-16 animate-fade-in">
         <ul className="flex items-center justify-center gap-4">
-          {Navigation.map((item) => (
-            <Link
-              key={item.href}
-              href={item.href}
-              className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
-            >
-              {item.name}
-            </Link>
-          ))}
+          
         </ul>
       </nav>
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
