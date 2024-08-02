77ac8a742dfd3204c69d261342771b074459c32f
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-02 10:21:55 +0300

    a

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -35,7 +35,7 @@ export const Navigation: React.FC = () => {
 							Atlikti Darbai
 						</Link>
 						<Link
-							href="/apiemus"
+							href="/apie-mus"
 							className="duration-200 text-zinc-400 hover:text-zinc-100"
 						>
 							Apie Mus
