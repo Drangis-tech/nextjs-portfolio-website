e70c8704d2300b4619798f402d87dd32a1abf332
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-03 17:54:06 +0300

    a

--- app/apie-mus/page.tsx
+++ app/apie-mus/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import React from "react";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
-import ParticlesComponent from "../components/Particles";
+import ParticlesComponent from "../components/particles";
 
 const aboutUsData = [
   {
