1a21ddec2676d7b6a3b72ded1c31a51d0bf5868c
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-03 14:20:52 +0300

    a

---
+++ data/about-us.ts
@@ -0,0 +1,16 @@
+const aboutUsData = [
+    {
+      title: "Mūsų komanda",
+      content: "Mes esame profesionalų komanda, pasiryžusi siekti jūsų tikslų."
+    },
+    {
+      title: "Mūsų vizija",
+      content: "Mūsų vizija yra tapti lyderiais savo srityje ir teikti aukščiausios kokybės paslaugas."
+    },
+    {
+      title: "Mūsų vertybės",
+      content: "Mes vertiname sąžiningumą, profesionalumą ir klientų pasitenkinimą."
+    }
+  ];
+  
+  export default aboutUsData;
