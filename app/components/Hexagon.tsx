d28910d8188f6a24444956204e827551392e67cd
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-06 18:32:06 +0300

    a

--- app/components/Hexagon.tsx
+++ app/components/Hexagon.tsx
@@ -1,7 +1,3 @@
-// File: components/Hexagon.tsx
-
-"use client";
-
 import React from 'react';
 
 interface Tech {
@@ -17,21 +13,34 @@ interface HexagonProps {
 
 const Hexagon: React.FC<HexagonProps> = ({ tech }) => {
   const hexagonStyle: React.CSSProperties = {
+    position: 'relative',
     width: '100px',
     height: '115px',
     backgroundColor: 'transparent',
     clipPath: 'polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%)',
     border: `5px solid ${tech.color}`,
     transition: 'transform 0.3s ease, background-color 0.3s ease, border-color 0.3s ease',
-    position: 'relative',
     boxSizing: 'border-box',
-    overflow: 'visible',
   };
 
   const hexagonHoverStyle: React.CSSProperties = {
     transform: 'scale(1.1)',
     backgroundColor: tech.color,
     borderColor: tech.color,
+  };
+
+  const handleMouseEnter = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const target = e.currentTarget as HTMLElement;
+    target.style.transform = hexagonHoverStyle.transform || '';
+    target.style.backgroundColor = hexagonHoverStyle.backgroundColor || '';
+    target.style.borderColor = hexagonHoverStyle.borderColor || '';
+  };
+
+  const handleMouseLeave = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const target = e.currentTarget as HTMLElement;
+    target.style.transform = '';
+    target.style.backgroundColor = 'transparent';
+    target.style.borderColor = tech.color;
   };
 
   const containerStyle: React.CSSProperties = {
@@ -48,16 +57,8 @@ const Hexagon: React.FC<HexagonProps> = ({ tech }) => {
     <div style={containerStyle}>
       <div
         style={hexagonStyle}
-        onMouseEnter={e => {
-          e.currentTarget.style.transform = hexagonHoverStyle.transform;
-          e.currentTarget.style.backgroundColor = hexagonHoverStyle.backgroundColor;
-          e.currentTarget.style.borderColor = hexagonHoverStyle.borderColor;
-        }}
-        onMouseLeave={e => {
-          e.currentTarget.style.transform = '';
-          e.currentTarget.style.backgroundColor = 'transparent';
-          e.currentTarget.style.borderColor = tech.color;
-        }}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
       >
         <a href={tech.url} target="_blank" rel="noopener noreferrer">
           <div className="flex items-center justify-center w-full h-full text-white">
