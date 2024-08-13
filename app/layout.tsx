924e086be10aec8b6d8397dbb71410152bc190db
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-13 18:56:35 +0300

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,7 +71,7 @@ export default function RootLayout({
           process.env.NODE_ENV === "development" ? "debug-screens" : undefined
         }`}
       >
-        <ClientLayout>{children}</ClientLayout>
+        {children}
       </body>
     </html>
   );
