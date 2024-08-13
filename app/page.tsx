38490c074fbea2faec13b9fad563bc60b1f16f2b
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-13 18:04:46 +0300

    a

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React from "react";
 import Particles from "./components/particles";
+import AnimatedIntro from "./components/AnimatedIntro"; // Import the AnimatedIntro component
 
 const navigation = [
   { name: "Atlikti Darbai", href: "/projects" },
@@ -29,9 +30,11 @@ export default function Home() {
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={100}
       />
-      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        Pavadinimas
-      </h1>
+      
+      {/* Replace the "PAVADINIMAS" text with the AnimatedIntro component */}
+      <div className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
+        <AnimatedIntro />
+      </div>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
@@ -41,5 +44,4 @@ export default function Home() {
       </div>
     </div>
   );
-
 }
