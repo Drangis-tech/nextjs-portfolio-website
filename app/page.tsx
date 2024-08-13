0e75657339386593d7136c0e9eba79bb137a3d7f
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-13 18:43:59 +0300

    a

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
         quantity={100}
       />
       
-      {/* Replace the "PAVADINIMAS" text with the AnimatedIntro component */}
+      {/* Replace the "Pavadinimas" text with the AnimatedIntro component */}
       <div className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
         <AnimatedIntro />
       </div>
