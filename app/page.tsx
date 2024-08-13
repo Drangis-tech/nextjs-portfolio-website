e2a69c89a631565e3478c66bc3d300fc17270b3a
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-13 18:45:41 +0300

    a

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import React from "react";
 import Particles from "./components/particles";
-import AnimatedIntro from "./components/AnimatedIntro"; // Import the AnimatedIntro component
 
 const navigation = [
   { name: "Atlikti Darbai", href: "/projects" },
@@ -30,11 +29,9 @@ export default function Home() {
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={100}
       />
-      
-      {/* Replace the "Pavadinimas" text with the AnimatedIntro component */}
-      <div className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
-        <AnimatedIntro />
-      </div>
+      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+        WebForge
+      </h1>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
@@ -44,4 +41,5 @@ export default function Home() {
       </div>
     </div>
   );
+
 }
