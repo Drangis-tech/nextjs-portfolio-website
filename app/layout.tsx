9bc9541bb9599fccd0ac5093cb1fa095f5134c1b
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-13 19:20:30 +0300

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,11 @@
 // app/layout.tsx
-"use client";  // Keep the use client directive
+"use client";
 
 import "../global.css";
 import { Inter } from "@next/font/google";
 import LocalFont from "@next/font/local";
 import { Analytics } from "./components/analytics";
 import Image from "next/image";
-import { useEffect, useState } from "react";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -23,17 +22,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const [showContent, setShowContent] = useState(false);
-
-  useEffect(() => {
-    // Simulate the animation delay
-    const timer = setTimeout(() => {
-      setShowContent(true);
-    }, 3000); // Adjust the time according to your animation duration
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
       <head>
@@ -44,23 +32,7 @@ export default function RootLayout({
           process.env.NODE_ENV === "development" ? "debug-screens" : undefined
         }`}
       >
-        {!showContent ? (
-          <div className="flex items-center justify-center h-screen">
-            {/* Logo and Animated WebForge Text */}
-            <div className="text-center animate-fade-in">
-              <Image
-                src="/logo.png"
-                alt="WebForge Logo"
-                width={150}
-                height={150}
-                className="mx-auto mb-4"
-              />
-              <h1 className="text-4xl text-white animate-bounce">WebForge</h1>
-            </div>
-          </div>
-        ) : (
-          children
-        )}
+        {children}
       </body>
     </html>
   );
