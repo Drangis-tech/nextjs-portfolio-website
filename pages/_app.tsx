eb8ef8a6fce8fa175bb730f0bdf837412b64954d
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-15 14:36:02 +0300

    a

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
-// File: pages/_app.tsx
+// pages/_app.tsx
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
-config.autoAddCss = false; // Prevents fontawesome from adding its CSS automatically
+config.autoAddCss = false; // Prevents FontAwesome from adding its CSS automatically
 
-import '../styles/globals.css';
+import '../styles/globals.css'; // Import global CSS
 
-import type { AppProps } from 'next/app'; // Import the type for AppProps
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp; // Adding FontAwesome icons to the project
+export default MyApp;
--- pages/_documents.tsx
+++ pages/_documents.tsx
@@ -4,10 +4,10 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html>
         <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          {/* No need to include FontAwesome CDN link here */}
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
         </Head>
         <body>
           <Main />
