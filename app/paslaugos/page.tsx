b7785d9c211de6f9ca9e70f927b80b57b18ccfa4
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-15 13:40:33 +0300

    a

--- app/paslaugos/page.tsx
+++ app/paslaugos/page.tsx
@@ -77,7 +77,7 @@ const Paslaugos: React.FC = () => {
   return (
     <div className="relative pb-16 bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
       <Navigation />
-      <div className="px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
+      <div className="px-6 pt-24 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-32 lg:pt-40">
         {/* Hero Section */}
         <div className="max-w-2xl mx-auto lg:mx-0">
           <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
