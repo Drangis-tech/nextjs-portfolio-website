ac1b6d1698d4b1ee14c4d57a85b2a00d32853c59
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-15 13:28:06 +0300

    a

--- app/paslaugos/page.tsx
+++ app/paslaugos/page.tsx
@@ -79,8 +79,10 @@ const Paslaugos: React.FC = () => {
       <Navigation />
       <div className="px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
         {/* Hero Section */}
-        <div className="max-w-2xl mx-auto lg:mx-0 text-center">
-          <h1 className="text-4xl font-bold tracking-tight text-zinc-100 sm:text-5xl">Mūsų Paslaugos</h1>
+        <div className="max-w-2xl mx-auto lg:mx-0">
+          <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
+            Mūsų Paslaugos
+          </h2>
           <p className="mt-4 text-zinc-400">
             Atraskite mūsų įvairias paslaugas, kurios padės jūsų verslui augti ir pasiekti naujas aukštumas.
           </p>
