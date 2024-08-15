a36b3112a16fca4f1bbc9df85c08fd128ec87ddc
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-15 14:27:48 +0300

    a

--- pages/_documents.tsx
+++ pages/_documents.tsx
@@ -4,9 +4,10 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          {/* No need to include FontAwesome CDN link here */}
         </Head>
         <body>
           <Main />
