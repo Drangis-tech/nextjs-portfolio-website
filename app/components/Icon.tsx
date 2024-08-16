7818f4206747cad2986887719254115f5f44f8b0
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-16 15:22:38 +0300

    a

--- app/components/Icon.tsx
+++ app/components/Icon.tsx
@@ -1,18 +1,16 @@
 // app/components/Icon.tsx
 import React from 'react';
-import { FontAwesomeIcon, SizeProp } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 interface IconProps {
   icon: IconProp;
-  size?: SizeProp;  // Correct type for size
+  size?: 'xs' | 'lg' | 'sm' | '1x' | '2x' | '3x' | '4x' | '5x' | '6x' | '7x' | '8x' | '9x' | '10x'; // Replace with your custom size type
   color?: string;
 }
 
 const Icon: React.FC<IconProps> = ({ icon, size = '1x', color = 'currentColor' }) => {
-  return (
-    <FontAwesomeIcon icon={icon} size={size} color={color} />
-  );
+  return <FontAwesomeIcon icon={icon} size={size} color={color} />;
 };
 
 export default Icon;
