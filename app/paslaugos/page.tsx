8ec4955d469f999e58c18d6cd9081dc0e817bd17
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-20 00:44:54 +0300

    a

--- app/paslaugos/page.tsx
+++ app/paslaugos/page.tsx
@@ -197,8 +197,8 @@ const Paslaugos = () => {
           <div className="mt-6">
             <a
               href="/contact"
-              className="inline-block bg-blue-600 text-white py-2 px-6 rounded-lg text-lg font-semibold hover:bg-blue-700"
-            >
+              className="w-full py-2 px-4 bg-gradient-to-r from-yellow-400 to-orange-500 text-white font-semibold rounded-md hover:from-yellow-300 hover:to-orange-400 active:from-yellow-500 active:to-orange-600 focus:outline-none focus:ring focus:ring-yellow-300 transition duration-200"
+              >
               Susisiekite
             </a>
           </div>
