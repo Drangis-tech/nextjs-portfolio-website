e209aba5a021c0dcf18c62dcacc2a987fba6fb72
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-20 00:57:47 +0300

    a

--- app/components/GradientButton.tsx
+++ app/components/GradientButton.tsx
@@ -15,10 +15,9 @@ const GradientButton: React.FC<GradientButtonProps> = ({ onClick, children }) =>
         border-2 border-transparent
         bg-gradient-to-r from-yellow-400 to-orange-500
         text-transparent bg-clip-text
-        hover:bg-gradient-to-r hover:from-yellow-300 hover:to-orange-400
-        hover:text-white
-        active:from-yellow-500 active:to-orange-600
-        focus:outline-none focus:ring-2 focus:ring-yellow-300
+        hover:bg-gradient-to-r hover:from-orange-400 hover:via-orange-500 hover:to-orange-600 hover:text-white
+        active:bg-gradient-to-r active:from-orange-500 active:via-orange-600 active:to-orange-700
+        focus:outline-none focus:ring-2 focus:ring-orange-400
         transition duration-200
         rounded-md
       "
