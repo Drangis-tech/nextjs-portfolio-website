a8200ee8b1591b5ef7232fb9b33714b4e9d60692
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-19 11:21:35 +0300

    a

--- app/components/Icon.tsx
+++ app/components/Icon.tsx
@@ -1,11 +1,11 @@
 // components/Icon.tsx
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { IconDefinition, SizeProp } from '@fortawesome/fontawesome-svg-core';
 
 interface IconProps {
   icon: IconDefinition;
-  size?: string;
+  size?: SizeProp; // Use SizeProp type
   color?: string;
 }
 
