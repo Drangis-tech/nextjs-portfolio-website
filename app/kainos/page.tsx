744ee190e603c67b64865a97f744a4b9c4ee348b
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-20 00:27:31 +0300

    a

--- app/kainos/page.tsx
+++ app/kainos/page.tsx
@@ -163,6 +163,7 @@ const PricingCalculator: React.FC = () => {
 const KainosPage: React.FC = () => {
   return (
     <div className="relative pb-16 bg-black">
+      <div className="absolute inset-0 -z-10 bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 opacity-20"></div>
       <Navigation />
       <div className="relative px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
         <div className="max-w-2xl mx-auto lg:mx-0">
