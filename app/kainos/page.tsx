648fdd8c80c46cccc207d15cd5a64ef9b88f7763
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-19 15:05:25 +0300

    a

--- app/kainos/page.tsx
+++ app/kainos/page.tsx
@@ -2,11 +2,11 @@
 
 import React from "react";
 import { Navigation } from "../components/nav";
-import PricingCalculator from "../components/PricingCalculator"; // Adjust the path as necessary
+import PricingCalculator from "./PricingCalculator"; // Adjust the path if necessary
 
 const KainosPage: React.FC = () => {
   return (
-    <div className="relative min-h-screen bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
+    <div className="relative pb-16 bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
       <Navigation />
       <div className="px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
         <div className="max-w-2xl mx-auto lg:mx-0">
