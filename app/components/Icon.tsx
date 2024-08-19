9e37dbaeee8ca22cd0e27f896776135b52bdb0a6
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-19 12:57:20 +0300

    a

--- app/components/Icon.tsx
+++ app/components/Icon.tsx
@@ -1,22 +1,16 @@
-// app/components/Icon.tsx
-import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { IconProp, SizeProp } from '@fortawesome/fontawesome-svg-core';
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { SizeProp } from '@fortawesome/fontawesome-svg-core'; // Correct import for SizeProp
+import React from 'react';
 
 interface IconProps {
-  icon: IconProp;
-  size?: SizeProp;
+  icon: IconDefinition;
+  size?: SizeProp;  // Correct type for the size prop
   color?: string;
 }
 
-const Icon: React.FC<IconProps> = ({ icon, size = '1x', color }) => {
-  return (
-    <FontAwesomeIcon
-      icon={icon}
-      size={size}
-      color={color}
-    />
-  );
+const Icon: React.FC<IconProps> = ({ icon, size = '2x', color = 'white' }) => {
+  return <FontAwesomeIcon icon={icon} size={size} style={{ color }} />;
 };
 
 export default Icon;
