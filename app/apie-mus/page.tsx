091ad8263d8bc800230b7c30197cad490c0b065f
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-20 18:09:42 +0300

    a

--- app/apie-mus/page.tsx
+++ app/apie-mus/page.tsx
@@ -68,9 +68,9 @@ const ApieMus: React.FC = () => {
                 <article className="relative w-full h-full p-6 group bg-[rgba(28, 28, 30, 0.8)] hover:bg-[rgba(44, 44, 46, 0.8)]">
                   <div className="text-xs text-zinc-100">
                     {/* Display the matching FontAwesome icon with a fixed size */}
-                    {section.title === "Mūsų Komanda" && <FontAwesomeIcon icon={faUsers} className="icon-small" />}
-                    {section.title === "Mūsų Vizija" && <FontAwesomeIcon icon={faEye} className="icon-small" />}
-                    {section.title === "Mūsų Vertybės" && <FontAwesomeIcon icon={faBalanceScale} className="icon-small" />}
+                    {section.title === "Mūsų Komanda" && <FontAwesomeIcon icon={faUsers} className="text-4xl" />}
+                    {section.title === "Mūsų Vizija" && <FontAwesomeIcon icon={faEye} className="text-4xl" />}
+                    {section.title === "Mūsų Vertybės" && <FontAwesomeIcon icon={faBalanceScale} className="text-4xl" />}
                   </div>
                   <h3 className="mt-4 text-2xl font-bold text-zinc-100 group-hover:text-white sm:text-3xl">
                     {section.title}
