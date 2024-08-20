c74cd94a47ecf604c01d8b13a801538dcc1542c2
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-20 14:28:46 +0300

    a

--- public/canvasAnimation.js
+++ public/canvasAnimation.js
@@ -1,162 +1,143 @@
 (function() {
   var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || 
-                  window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || function( callback ){
-          window.setTimeout(callback, 1000 / 60);
-        };
+                window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || function( callback ){
+        window.setTimeout(callback, 1000 / 60);
+      };
   window.requestAnimationFrame = requestAnimationFrame;
 })();
 
-// Get canvas elements and contexts
+// Terrain stuff.
 var terrain = document.getElementById("terCanvas"),
-  background = document.getElementById("bgCanvas"),
-  terCtx = terrain.getContext("2d"),
-  bgCtx = background.getContext("2d"),
-  width = window.innerWidth,
-  height = window.innerHeight;
+    background = document.getElementById("bgCanvas"),
+    terCtx = terrain.getContext("2d"),
+    bgCtx = background.getContext("2d"),
+    width = window.innerWidth,
+    height = document.body.offsetHeight;
+(height < 400)?height = 400:height;
 
-// Set canvas sizes
 terrain.width = background.width = width;
 terrain.height = background.height = height;
 
-// Variables for terrain
+// Some random points
 var points = [],
-  displacement = 140,
-  power = Math.pow(2, Math.ceil(Math.log(width) / Math.log(2)));
+    displacement = 140,
+    power = Math.pow(2,Math.ceil(Math.log(width)/(Math.log(2))));
 
-// Function to calculate terrain height
-function calculateTerrainHeight() {
-  // Calculate terrain height based on the viewport height
-  return height * 0.6; // Adjust the factor as needed
+// set the start height and end height for the terrain
+points[0] = (height - (Math.random()*height/2))-displacement;
+points[power] = (height - (Math.random()*height/2))-displacement;
+
+// create the rest of the points
+for(var i = 1; i<power; i*=2){
+  for(var j = (power/i)/2; j <power; j+=power/i){
+    points[j] = ((points[j - (power/i)/2] + points[j + (power/i)/2]) / 2) + Math.floor(Math.random()*-displacement+displacement );
+  }
+  displacement *= 0.6;
 }
 
-// Set the start height and end height for the terrain
-function drawTerrain() {
-  var terrainHeight = calculateTerrainHeight();
-  points[0] = (height - (Math.random() * height / 2)) - displacement;
-  points[power] = (height - (Math.random() * height / 2)) - displacement;
-
-  for (var i = 1; i < power; i *= 2) {
-      for (var j = (power / i) / 2; j < power; j += power / i) {
-          points[j] = ((points[j - (power / i) / 2] + points[j + (power / i) / 2]) / 2) + Math.floor(Math.random() * -displacement + displacement);
-      }
-      displacement *= 0.6;
+// draw the terrain
+terCtx.beginPath();
+for(var i = 0; i<=width; i++){
+  if(i === 0){
+    terCtx.moveTo(0, points[0]);
+  }else if(points[i] !== undefined){
+    terCtx.lineTo(i, points[i]);
   }
-
-  // Draw the terrain
-  terCtx.beginPath();
-  for (var i = 0; i <= width; i++) {
-      if (i === 0) {
-          terCtx.moveTo(0, points[0]);
-      } else if (points[i] !== undefined) {
-          terCtx.lineTo(i, points[i]);
-      }
-  }
-  terCtx.lineTo(width, height - (height - terrainHeight)); // Lower the terrain based on the calculated height
-  terCtx.lineTo(0, height - (height - terrainHeight)); // Lower the terrain based on the calculated height
-  terCtx.lineTo(0, points[0]);
-  terCtx.closePath();
-  terCtx.fillStyle = "#333"; // Darker gray for the terrain
-  terCtx.fill();
 }
+terCtx.lineTo(width,terrain.height);
+terCtx.lineTo(0,terrain.height);
+terCtx.lineTo(0,points[0]);
+terCtx.fill();
 
-// Draw stars on the background canvas
-bgCtx.fillStyle = '#000'; // Background color of stars canvas
-bgCtx.fillRect(0, 0, width, height);
+// Second canvas used for the stars
+bgCtx.fillStyle = '#05004c';
+bgCtx.fillRect(0,0,width,height);
 
-// Star and shooting star definitions
-function Star(options) {
-  this.size = Math.random() * 2;
-  this.speed = Math.random() * 0.1;
+// stars
+function Star(options){
+  this.size = Math.random()*2;
+  this.speed = Math.random()*.1;
   this.x = options.x;
   this.y = options.y;
 }
 
-Star.prototype.reset = function () {
-  this.size = Math.random() * 2;
-  this.speed = Math.random() * 0.1;
+Star.prototype.reset = function(){
+  this.size = Math.random()*2;
+  this.speed = Math.random()*.1;
   this.x = width;
-  this.y = Math.random() * height;
+  this.y = Math.random()*height;
 }
 
-Star.prototype.update = function () {
+Star.prototype.update = function(){
   this.x -= this.speed;
-  if (this.x < 0) {
-      this.reset();
+  if(this.x < 0){
+    this.reset();
   } else {
-      bgCtx.fillStyle = '#ffffff'; // White color for stars
-      bgCtx.fillRect(this.x, this.y, this.size, this.size);
+    bgCtx.fillRect(this.x, this.y, this.size, this.size); 
   }
 }
 
-function ShootingStar() {
+function ShootingStar(){
   this.reset();
 }
 
-ShootingStar.prototype.reset = function () {
-  this.x = Math.random() * width;
+ShootingStar.prototype.reset = function(){
+  this.x = Math.random()*width;
   this.y = 0;
-  this.len = (Math.random() * 80) + 10;
-  this.speed = (Math.random() * 10) + 6;
-  this.size = (Math.random() * 1) + 0.1;
-  this.waitTime = new Date().getTime() + (Math.random() * 3000) + 500;
+  this.len = (Math.random()*80)+10;
+  this.speed = (Math.random()*10)+6;
+  this.size = (Math.random()*1)+0.1;
+  this.waitTime =  new Date().getTime() + (Math.random()*3000)+500;
   this.active = false;
 }
 
-ShootingStar.prototype.update = function () {
-  if (this.active) {
-      this.x -= this.speed;
-      this.y += this.speed;
-      if (this.x < 0 || this.y >= height) {
-          this.reset();
-      } else {
-          bgCtx.lineWidth = this.size;
-          bgCtx.beginPath();
-          bgCtx.moveTo(this.x, this.y);
-          bgCtx.lineTo(this.x + this.len, this.y - this.len);
-          bgCtx.strokeStyle = '#ffffff'; // White color for shooting stars
-          bgCtx.stroke();
-      }
+ShootingStar.prototype.update = function(){
+  if(this.active){
+    this.x -= this.speed;
+    this.y += this.speed;
+    if(this.x < 0 || this.y >= height){
+      this.reset();
+    } else {
+      bgCtx.lineWidth = this.size;
+      bgCtx.beginPath();
+      bgCtx.moveTo(this.x, this.y);
+      bgCtx.lineTo(this.x+this.len, this.y-this.len);
+      bgCtx.stroke();
+    }
   } else {
-      if (this.waitTime < new Date().getTime()) {
-          this.active = true;
-      }
+    if(this.waitTime < new Date().getTime()){
+      this.active = true;
+    }			
   }
 }
 
 var entities = [];
-for (var i = 0; i < height; i++) {
-  entities.push(new Star({ x: Math.random() * width, y: Math.random() * height }));
+
+// Adjust number of stars based on screen width
+var numStars = (width > 768) ? height : Math.max(height / 2, 50);
+
+// init the stars
+for(var i = 0; i < numStars; i++){
+  entities.push(new Star({x:Math.random()*width, y:Math.random()*height}));
 }
+
+// Add 2 shooting stars that just cycle.
 entities.push(new ShootingStar());
 entities.push(new ShootingStar());
 
-// Animate the stars
-function animate() {
-  bgCtx.fillStyle = '#000'; // Background color of stars canvas
-  bgCtx.fillRect(0, 0, width, height);
+//animate background
+function animate(){
+  bgCtx.fillStyle = '#05004c';
+  bgCtx.fillRect(0,0,width,height);
+  bgCtx.fillStyle = '#ffffff';
+  bgCtx.strokeStyle = '#ffffff';
+
   var entLen = entities.length;
-  while (entLen--) {
-      entities[entLen].update();
+  
+  while(entLen--){
+    entities[entLen].update();
   }
+  
   requestAnimationFrame(animate);
 }
 animate();
-
-// Handle window resize
-window.addEventListener("resize", function() {
-  width = window.innerWidth;
-  height = window.innerHeight;
-  terrain.width = background.width = width;
-  terrain.height = background.height = height;
-  terCtx.clearRect(0, 0, width, height);
-  bgCtx.clearRect(0, 0, width, height);
-
-  drawTerrain(); // Redraw terrain on resize
-
-  bgCtx.fillStyle = '#000'; // Background color of stars canvas
-  bgCtx.fillRect(0, 0, width, height);
-
-  entities.forEach(entity => entity.reset());
-  entities.push(new ShootingStar());
-  entities.push(new ShootingStar());
-});
