91724c025f89b8909f870c0ec0cbc1a7059fb61e
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-22 13:27:15 +0300

    a

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,40 +1,152 @@
-// ../components/nav.tsx
-import React, { useState } from 'react';
-import Link from 'next/link';
+"use client";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+import React, { useEffect, useRef, useState } from "react";
+import { usePathname } from 'next/navigation';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
-const navigation = [
-  { name: "Atlikti Darbai", href: "/projects" },
-  { name: "Paslaugos", href: "/paslaugos" },
-  { name: "Apie Mus", href: "/apie-mus" },
-  { name: "Kainos", href: "/kainos" },
-  { name: "Kontaktai", href: "/contact" },
-];
+export const Navigation: React.FC = () => {
+  const ref = useRef<HTMLElement>(null);
+  const overlayRef = useRef<HTMLDivElement>(null);
+  const [isIntersecting, setIntersecting] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
 
-export const Navigation: React.FC<{ isOpen: boolean; onMenuToggle: () => void }> = ({ isOpen, onMenuToggle }) => {
-  const handleMenuToggle = () => {
-    onMenuToggle();
+  useEffect(() => {
+    if (!ref.current) return;
+    const observer = new IntersectionObserver(([entry]) =>
+      setIntersecting(entry.isIntersecting)
+    );
+
+    observer.observe(ref.current);
+    return () => observer.disconnect();
+  }, []);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (isOpen && overlayRef.current && !overlayRef.current.contains(event.target as Node) && !ref.current?.contains(event.target as Node)) {
+        setIsOpen(false);
+        document.body.style.overflow = 'auto';
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isOpen]);
+
+  const toggleMenu = () => {
+    setIsOpen(prevState => {
+      const newState = !prevState;
+      document.body.style.overflow = newState ? 'hidden' : 'auto';
+      return newState;
+    });
   };
 
   return (
-    <nav>
-      <button onClick={handleMenuToggle} className="md:hidden">
-        <FontAwesomeIcon icon={isOpen ? faTimes : faBars} size="2x" />
-      </button>
-      <div className={`fixed inset-0 bg-gray-800/75 md:hidden ${isOpen ? 'block' : 'hidden'}`}>
-        <ul className="flex flex-col items-center justify-center h-full space-y-4">
-          {navigation.map((item) => (
-            <li key={item.href}>
-              <Link href={item.href}>
-                <a className="text-white text-lg" onClick={() => onMenuToggle()}>
-                  {item.name}
-                </a>
+    <header ref={ref} className="relative">
+      {/* Main Header */}
+      <div
+        className={`fixed inset-x-0 top-0 z-50 backdrop-blur-sm duration-200 border-b ${
+          isIntersecting
+            ? "bg-zinc-900/0 border-transparent"
+            : "bg-zinc-900/500 border-zinc-800"
+        }`}
+      >
+        <div className="container flex items-center p-6 mx-auto">
+          {pathname !== '/' && (
+            <Link
+              href="/"
+              className={`flex items-center text-zinc-300 hover:text-zinc-100 mr-4 transition-opacity duration-300 ease-in-out ${isOpen ? 'opacity-0' : 'opacity-100'}`}
+            >
+              <ArrowLeft className="w-8 h-8 md:w-10 md:h-10" />
+            </Link>
+          )}
+
+          <div className="flex-grow flex items-center justify-center">
+            <div className="hidden md:flex gap-8">
+              <Link
+                href="/projects"
+                className="duration-200 text-zinc-400 hover:text-zinc-100"
+              >
+                Atlikti Darbai
               </Link>
-            </li>
-          ))}
-        </ul>
+              <Link
+                href="/paslaugos"
+                className="duration-200 text-zinc-400 hover:text-zinc-100"
+              >
+                Paslaugos
+              </Link>
+              <Link
+                href="/apie-mus"
+                className="duration-200 text-zinc-400 hover:text-zinc-100"
+              >
+                Apie Mus
+              </Link>
+              <Link
+                href="/kainos"
+                className="duration-200 text-zinc-400 hover:text-zinc-100"
+              >
+                Kainos
+              </Link>
+              <Link
+                href="/contact"
+                className="duration-200 text-zinc-400 hover:text-zinc-100"
+              >
+                Kontaktai
+              </Link>
+            </div>
+          </div>
+
+          <button
+            className={`flex items-center text-zinc-300 hover:text-zinc-100 md:hidden transition-opacity duration-300 ease-in-out ${isOpen ? 'opacity-0' : 'opacity-100'}`}
+            onClick={toggleMenu}
+          >
+            <FontAwesomeIcon icon={faBars} className="w-8 h-8 md:w-10 md:h-10" />
+          </button>
+        </div>
       </div>
-    </nav>
+
+      {/* Backdrop Blur Overlay */}
+      <div
+        ref={overlayRef}
+        className={`fixed inset-0 bg-black bg-opacity-70 backdrop-blur-sm transition-opacity duration-300 ease-in-out ${isOpen ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'}`}
+        onClick={() => isOpen && setIsOpen(false)}
+      ></div>
+
+      {/* Side Menu */}
+      <div className={`fixed inset-y-0 right-0 w-3/4 bg-black bg-opacity-80 backdrop-blur-sm transition-transform duration-300 ease-in-out transform ${isOpen ? 'translate-x-0' : 'translate-x-full'} z-50`}>
+        <div className="relative w-full h-full flex flex-col items-start justify-center space-y-8 px-8">
+          <button
+            className="absolute top-4 right-4 text-zinc-300 hover:text-zinc-100 w-10 h-10 flex items-center justify-center"
+            onClick={toggleMenu}
+          >
+            <FontAwesomeIcon icon={faTimes} className="w-8 h-8" />
+          </button>
+
+          <nav className="flex flex-col items-start space-y-6">
+            <Link href="/paslaugos" className="text-white text-3xl transition-transform duration-300 ease-in-out hover:text-gradient hover:scale-105" onClick={toggleMenu}>
+              Paslaugos
+            </Link>
+            <Link href="/apie-mus" className="text-white text-3xl transition-transform duration-300 ease-in-out hover:text-gradient hover:scale-105" onClick={toggleMenu}>
+              Apie Mus
+            </Link>
+            <Link href="/kainos" className="text-white text-3xl transition-transform duration-300 ease-in-out hover:text-gradient hover:scale-105" onClick={toggleMenu}>
+              Kainos
+            </Link>
+            <Link href="/kontaktai" className="text-white text-3xl transition-transform duration-300 ease-in-out hover:text-gradient hover:scale-105" onClick={toggleMenu}>
+              Kontaktai
+            </Link>
+          </nav>
+        </div>
+      </div>
+    </header>
   );
 };
