a5cfb022d1babe70ab9f8bc7af3225abfb6bd838
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-22 22:10:15 +0300

    a

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -139,6 +139,14 @@ export const Navigation: React.FC = () => {
             </button>
           </div>
 
+          {/* Separator Line with Gaps */}
+          <div className="relative px-6 py-4 border-b border-gray-200">
+            <h1 className="text-3xl font-bold text-center">Paslaugos</h1>
+            <div className="absolute inset-0 flex items-center">
+              <hr className="w-1/3 border-gray-300 border-t-2 mx-auto" />
+            </div>
+          </div>
+
           <nav className="flex flex-col items-start space-y-6 p-6">
             <Link href="/paslaugos" className="text-white text-3xl transition-transform duration-300 ease-in-out hover:text-gradient hover:scale-105" onClick={toggleMenu}>
               Paslaugos
