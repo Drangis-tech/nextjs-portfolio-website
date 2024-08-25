d1f7e5afd6cdae5a23252bce97e18cf86d659005
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-25 12:55:00 +0300

    a

--- public/canvasAnimation.js
+++ public/canvasAnimation.js
@@ -1,6 +1,6 @@
 (function() {
   var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || 
-                window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || function( callback ){
+                window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || function(callback) {
         window.setTimeout(callback, 1000 / 60);
       };
   window.requestAnimationFrame = requestAnimationFrame;
@@ -13,7 +13,7 @@ var terrain = document.getElementById("terCanvas"),
     bgCtx = background.getContext("2d"),
     width = window.innerWidth,
     height = document.body.offsetHeight;
-(height < 400)?height = 400:height;
+(height < 400) ? height = 400 : height;
 
 terrain.width = background.width = width;
 terrain.height = background.height = height;
@@ -21,93 +21,99 @@ terrain.height = background.height = height;
 // Some random points
 var points = [],
     displacement = 140,
-    power = Math.pow(2,Math.ceil(Math.log(width)/(Math.log(2))));
+    power = Math.pow(2, Math.ceil(Math.log(width) / (Math.log(2))));
 
-// set the start height and end height for the terrain
-points[0] = (height - (Math.random()*height/2))-displacement;
-points[power] = (height - (Math.random()*height/2))-displacement;
+// Set the start height and end height for the terrain
+points[0] = (height - (Math.random() * height / 2)) - displacement;
+points[power] = (height - (Math.random() * height / 2)) - displacement;
 
-// create the rest of the points
-for(var i = 1; i<power; i*=2){
-  for(var j = (power/i)/2; j <power; j+=power/i){
-    points[j] = ((points[j - (power/i)/2] + points[j + (power/i)/2]) / 2) + Math.floor(Math.random()*-displacement+displacement );
+// Create the rest of the points
+for (var i = 1; i < power; i *= 2) {
+  for (var j = (power / i) / 2; j < power; j += power / i) {
+    points[j] = ((points[j - (power / i) / 2] + points[j + (power / i) / 2]) / 2) + Math.floor(Math.random() * -displacement + displacement);
   }
   displacement *= 0.6;
 }
 
-// draw the terrain
+// Draw the terrain
+terCtx.fillStyle = '#000000'; // Set mountains color to black
 terCtx.beginPath();
-for(var i = 0; i<=width; i++){
-  if(i === 0){
+for (var i = 0; i <= width; i++) {
+  if (i === 0) {
     terCtx.moveTo(0, points[0]);
-  }else if(points[i] !== undefined){
+  } else if (points[i] !== undefined) {
     terCtx.lineTo(i, points[i]);
   }
 }
-terCtx.lineTo(width,terrain.height);
-terCtx.lineTo(0,terrain.height);
-terCtx.lineTo(0,points[0]);
+terCtx.lineTo(width, terrain.height);
+terCtx.lineTo(0, terrain.height);
+terCtx.lineTo(0, points[0]);
 terCtx.fill();
 
-// Second canvas used for the stars
-bgCtx.fillStyle = '#05004c';
-bgCtx.fillRect(0,0,width,height);
+// Add a white outline to the mountains
+terCtx.strokeStyle = '#D3D3D3'; // Set the outline color to grey (use '#FFFFFF' for white)
+terCtx.lineWidth = 2; // Set the outline width
+terCtx.stroke();
 
-// stars
-function Star(options){
-  this.size = Math.random()*2;
-  this.speed = Math.random()*.1;
+// Second canvas used for the stars
+bgCtx.fillStyle = '#003366'; // Change the sky color to a different blue
+bgCtx.fillRect(0, 0, width, height);
+
+// Stars
+function Star(options) {
+  this.size = Math.random() * 2;
+  this.speed = Math.random() * .1;
   this.x = options.x;
   this.y = options.y;
 }
 
-Star.prototype.reset = function(){
-  this.size = Math.random()*2;
-  this.speed = Math.random()*.1;
+Star.prototype.reset = function() {
+  this.size = Math.random() * 2;
+  this.speed = Math.random() * .1;
   this.x = width;
-  this.y = Math.random()*height;
+  this.y = Math.random() * height;
 }
 
-Star.prototype.update = function(){
+Star.prototype.update = function() {
   this.x -= this.speed;
-  if(this.x < 0){
+  if (this.x < 0) {
     this.reset();
   } else {
-    bgCtx.fillRect(this.x, this.y, this.size, this.size); 
+    bgCtx.fillRect(this.x, this.y, this.size, this.size);
   }
 }
 
-function ShootingStar(){
+function ShootingStar() {
   this.reset();
 }
 
-ShootingStar.prototype.reset = function(){
-  this.x = Math.random()*width;
+ShootingStar.prototype.reset = function() {
+  this.x = Math.random() * width;
   this.y = 0;
-  this.len = (Math.random()*80)+10;
-  this.speed = (Math.random()*10)+6;
-  this.size = (Math.random()*1)+0.1;
-  this.waitTime =  new Date().getTime() + (Math.random()*3000)+500;
+  this.len = (Math.random() * 80) + 10;
+  this.speed = (Math.random() * 10) + 6;
+  this.size = (Math.random() * 1) + 0.1;
+  this.waitTime = new Date().getTime() + (Math.random() * 3000) + 500;
   this.active = false;
 }
 
-ShootingStar.prototype.update = function(){
-  if(this.active){
+ShootingStar.prototype.update = function() {
+  if (this.active) {
     this.x -= this.speed;
     this.y += this.speed;
-    if(this.x < 0 || this.y >= height){
+    if (this.x < 0 || this.y >= height) {
       this.reset();
     } else {
       bgCtx.lineWidth = this.size;
       bgCtx.beginPath();
       bgCtx.moveTo(this.x, this.y);
-      bgCtx.lineTo(this.x+this.len, this.y-this.len);
+      bgCtx.lineTo(this.x + this.len, this.y - this.len);
       bgCtx.stroke();
     }
   } else {
-    if(this.waitTime < new Date().getTime()){
+    if (this.waitTime < new Date().getTime()) {
       this.active = true;
-    }			
+    }
   }
 }
 
@@ -116,28 +122,28 @@ var entities = [];
 // Adjust number of stars based on screen width
 var numStars = (width > 768) ? height : Math.max(height / 2, 50);
 
-// init the stars
-for(var i = 0; i < numStars; i++){
-  entities.push(new Star({x:Math.random()*width, y:Math.random()*height}));
+// Init the stars
+for (var i = 0; i < numStars; i++) {
+  entities.push(new Star({ x: Math.random() * width, y: Math.random() * height }));
 }
 
 // Add 2 shooting stars that just cycle.
 entities.push(new ShootingStar());
 entities.push(new ShootingStar());
 
-//animate background
-function animate(){
-  bgCtx.fillStyle = '#05004c';
-  bgCtx.fillRect(0,0,width,height);
+// Animate background
+function animate() {
+  bgCtx.fillStyle = '#003366'; // Keep the sky color consistent during animation
+  bgCtx.fillRect(0, 0, width, height);
   bgCtx.fillStyle = '#ffffff';
   bgCtx.strokeStyle = '#ffffff';
 
   var entLen = entities.length;
-  
-  while(entLen--){
+
+  while (entLen--) {
     entities[entLen].update();
   }
-  
+
   requestAnimationFrame(animate);
 }
 animate();
