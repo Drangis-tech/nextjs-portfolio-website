f9a92aabc8840310517607c6ea35c236a8d32a4b
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-26 11:23:53 +0300

    a

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,18 @@
 // pages/_app.tsx
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
-config.autoAddCss = false; // Prevents FontAwesome from adding its CSS automatically
-
+config.autoAddCss = false; // Prevent FontAwesome from adding its CSS automatically
 
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import BaseLayout from '../components/BaseLayout'; // Import your layout component
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <BaseLayout>
+      <Component {...pageProps} />
+    </BaseLayout>
+  );
 }
 
 export default MyApp;
