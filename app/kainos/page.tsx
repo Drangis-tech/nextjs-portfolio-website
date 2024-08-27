06512557981bd83ae2cafa276d51e9032b282a9d
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-27 17:13:14 +0300

    a

--- app/kainos/page.tsx
+++ app/kainos/page.tsx
@@ -145,9 +145,12 @@ const KainosPage: React.FC = () => {
   return (
     <div className="relative pb-16 bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
       <Navigation />
-      <div className="px-6 pt-24 mx-auto max-w-7xl lg:px-8 md:pt-32 lg:pt-40">
+      <div className="px-6 pt-24 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-32 lg:pt-40">
+        {/* Hero Section */}
         <div className="max-w-2xl mx-auto lg:mx-0">
-          <h2 className="text-3xl font-bold text-zinc-100 mb-6">Kainos</h2>
+          <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl mb-6">
+            Kainos
+          </h2>
           <p className="text-base text-zinc-400 mt-4">
             Sužinokite apytikslę savo svetainės kūrimo kainą naudodamiesi mūsų kainų skaičiuokle.
           </p>
