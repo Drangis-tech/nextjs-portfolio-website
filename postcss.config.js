643b5480aaab5c690149df5807ec0d54451ca845
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-27 09:27:59 +0000

    a

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	plugins: {
-	  'postcss-import': {},        // Ensure you import other CSS files
-	  'postcss-nesting': {},       // Optional: Use this for nesting support
-	  tailwindcss: {},             // Tailwind CSS plugin
-	  autoprefixer: {},            // Adds vendor prefixes
+	  'postcss-import': {},
+	  'postcss-nesting': {}, // Changed from tailwindcss/nesting to postcss-nesting
+	  tailwindcss: {},
+	  autoprefixer: {},
 	},
   };
