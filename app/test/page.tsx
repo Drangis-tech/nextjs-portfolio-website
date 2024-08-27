35fe0015a79373ec794fc3a2ae462d12ba51d6e0
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-27 13:36:59 +0300

    a

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -5,12 +5,9 @@ import styles from './page.module.css'; // Import the updated CSS module
 const TestPage = () => {
   return (
     <div className={styles['background-container']}>
-      <div className={styles['white-dots']}></div>
-      <div className={styles['gradient-figures']}>
-        <div className={styles.circle}></div> {/* Circle outline */}
-        <div className={styles.square}></div> {/* Square outline */}
-        <div className={styles.triangle}></div> {/* Triangle outline */}
-      </div>
+      <div className={styles['shape'] + ' ' + styles.circle}></div> {/* Circle outline */}
+      <div className={styles['shape'] + ' ' + styles.square}></div> {/* Square outline */}
+      <div className={styles['shape'] + ' ' + styles.triangle}></div> {/* Triangle outline */}
       <h1 className={styles.title}>Test Page</h1>
       <p className={styles.description}>This is a test page with custom background effects.</p>
     </div>
