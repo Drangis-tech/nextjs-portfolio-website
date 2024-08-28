ab71e20f86f1dea2f69e90fd4725a19c69f11c37
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-08-28 14:31:27 +0300

    a

--- app/kainos/page.tsx
+++ app/kainos/page.tsx
@@ -155,9 +155,9 @@ const PricingCalculator: React.FC = () => {
 
 const KainosPage: React.FC = () => {
   return (
-    <div className="relative pb-16 bg-gradient-to-tl from-black via-zinc-900 to-black">
+    <div className="relative pb-16 bg-gradient-to-b from-black via-zinc-900 to-black">
       <Navigation />
-      <div className="px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
+      <div className="relative px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
         {/* Hero Section */}
         <div className="max-w-2xl mx-auto lg:mx-0 text-center lg:text-left">
           <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
