0e397f3e74f504b63a6718676dd9ff3a1b036f90
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-09-01 10:22:08 +0300

    a

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,22 @@
 // pages/_app.tsx
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
-config.autoAddCss = false; // Prevents FontAwesome from adding its CSS automatically
-
-
+import Head from 'next/head';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
+config.autoAddCss = false; // Prevents FontAwesome from adding its CSS automatically
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
