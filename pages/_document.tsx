41687d2068802210d40b89da1d15168fd74114e3
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-09-02 13:26:07 +0300

    a

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,20 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
           />
-          <link rel="icon" type="image/png" href="/favicon.ico" />
+
+          {/* Favicon and related icons */}
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <link rel="icon" sizes="192x192" href="/android-chrome-192x192.png" />
+          <link rel="icon" sizes="512x512" href="/android-chrome-512x512.png" />
+          <meta name="msapplication-TileColor" content="#000000" />
+          <meta name="msapplication-config" content="/browserconfig.xml" />
+          <meta name="theme-color" content="#000000" />
+
+          {/* Other head elements like fonts or CSS files */}
         </Head>
         <body>
           <Main />
