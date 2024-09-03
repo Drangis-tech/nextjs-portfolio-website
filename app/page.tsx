9f676f43ae3021cd1c61f2895670997c5feeae24
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-09-03 14:57:55 +0300

    a

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,35 @@
-// pages/index.tsx
 "use client";
 
-import Link from 'next/link';
+import Link from 'next/link'; // Import Link from next/link
 import React, { useEffect } from 'react';
 import Image from 'next/image';
-import Head from 'next/head';
-import { Navigation } from './components/nav';
+import Head from 'next/head'; // Import Head from next/head
+import { Navigation } from './components/nav'; // Import Navigation
+
+const navigation = [
+  { name: "Atlikti Darbai", href: "/projects" },
+  { name: "Paslaugos", href: "/paslaugos" },
+  { name: "Apie Mus", href: "/apie-mus" },
+  { name: "Kainos", href: "/kainos" },
+  { name: "Kontaktai", href: "/contact" },
+];
 
 export default function Home() {
   useEffect(() => {
+    // Add no-scroll class to body
     document.body.classList.add('no-scroll');
+
+    // Prevent default touch actions on mobile
     const preventDefault = (e: Event) => e.preventDefault();
     document.body.addEventListener('touchmove', preventDefault, { passive: false });
 
+    // Insert the JavaScript code for the canvas animation here
     const script = document.createElement("script");
-    script.src = "/canvasAnimation.js";
+    script.src = "/canvasAnimation.js"; // Move the JS code to a separate file (canvasAnimation.js)
     script.async = true;
     document.body.appendChild(script);
 
+    // Cleanup function to remove no-scroll class, preventDefault event, and script
     return () => {
       document.body.classList.remove('no-scroll');
       document.body.removeEventListener('touchmove', preventDefault);
@@ -28,15 +40,18 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Home | brandforge.lt</title>
+        <title>Home | brandforge.lt</title> {/* Set the title for the page */}
       </Head>
       
       <div className="relative flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-black">
         <canvas id="bgCanvas" className="absolute top-0 left-0" />
         <canvas id="terCanvas" className="absolute top-0 left-0" />
+
+        {/* Include Navigation component with a hidden back button */}
         <header className="w-full">
           <Navigation />
         </header>
+
         <div className="flex flex-col items-center justify-center z-10">
           <div className="flex flex-col items-center">
             <Image
@@ -54,6 +69,7 @@ export default function Home() {
             </h1>
           </div>
         </div>
+
         <div className="hidden w-screen h-px md:block animate-glow bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
         <div className="my-16 text-center animate-fade-in">
           <h2 className="text-sm text-white shine-effect">
