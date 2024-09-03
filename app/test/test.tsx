ddcb533d686529234a1534eb2e40400be0e509e7
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-09-03 14:43:03 +0300

    a

---
+++ app/test/test.tsx
@@ -0,0 +1,13 @@
+// pages/test.tsx
+import Head from 'next/head';
+
+export default function TestPage() {
+  return (
+    <>
+      <Head>
+        <title>Test Page Title</title>
+      </Head>
+      <h1>This is a test page</h1>
+    </>
+  );
+}
