b777b0779137ff95f5c3d895fa67a2a6ab9253f4
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-09-04 23:03:29 +0300

    a

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
     <div
       onMouseMove={onMouseMove}
       className="relative duration-700 border rounded-xl hover:bg-zinc-800/10 group hover:border-zinc-400/50 border-zinc-600 overflow-hidden"
-      style={{ height: '250px', width: '300px' }} // Increased width by 20%
+      style={{ height: '250px', width: '360px' }} // Increased width by 20%
     >
       <div className="pointer-events-none absolute inset-0 z-10">
         <div className="absolute inset-0 transition duration-1000 [mask-image:linear-gradient(black,transparent)]" />
@@ -65,10 +65,10 @@ export default function Home() {
         </header>
 
         <div className="relative flex flex-col items-center justify-center text-center text-white px-4 md:px-8 flex-1 z-20 mt-32 md:mt-32">
-          <h2 className="text-3xl md:text-5xl lg:text-6xl font-bold mb-4 md:mb-6 leading-tight">
+          <h2 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4 md:mb-6 leading-tight">
             Build, host, and scale your<br className="hidden md:block"/> collaborative product
           </h2>
-          <p className="text-lg text-gray-400 mb-6 md:mb-8">
+          <p className="text-lg md:text-xl text-gray-400 mb-6 md:mb-8">
             The best way to reach humans instead of spam folders. Deliver transactional and marketing emails at scale.
           </p>
           <div className="mb-6 md:mb-8">
