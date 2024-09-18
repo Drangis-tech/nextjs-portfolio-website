48380fdac6668e331318bbfcf7eaf294fbd238a4
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-09-18 23:49:36 +0300

    Projektai

--- app/components/ui/hero-parallax.tsx
+++ app/components/ui/hero-parallax.tsx
@@ -116,12 +116,11 @@ export const Header = () => {
   return (
     <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full left-0 top-0">
       <h1 className="text-2xl md:text-7xl font-bold dark:text-white">
-        The Ultimate <br /> development studio
+        Mes <br /> Brandforge komanda
       </h1>
       <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
-        We build beautiful products with the latest technologies and frameworks.
-        We are a team of passionate developers and designers that love to build
-        amazing products.
+      Kuriame gražius ir funkcionalius puslapius, naudodami įvairias technologijas.
+      Siekiame greito darbo, patrauklaus dizaino, praktiškumo ir kainų, nuo kurių neišsigąsta piniginė.
       </p>
     </div>
   );
