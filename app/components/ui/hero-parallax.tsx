162282904801e08e4d30e5bcdd4608124b257910
Drangis-tech <nojusmykolaitis04@gmail.com>
2024-10-01 18:36:48 +0300

    grid backgroundas

--- app/components/ui/hero-parallax.tsx
+++ app/components/ui/hero-parallax.tsx
@@ -59,7 +59,7 @@ export const HeroParallax: React.FC<{ products: { title: string; link: string; t
   return (
     <div
       ref={ref}
-      className="relative h-[150vh] py-12 overflow-hidden antialiased flex flex-col [perspective:1000px] [transform-style:preserve-3d] z-20" // Adjusted height and padding
+      className="relative h-[150vh] py-12 overflow-x-hidden overflow-y-visible antialiased flex flex-col [perspective:1000px] [transform-style:preserve-3d] z-20" // Overflow adjustments
     >
       <Header />
       <motion.div
@@ -71,7 +71,7 @@ export const HeroParallax: React.FC<{ products: { title: string; link: string; t
         }}
         className="relative z-30"
       >
-        <motion.div className="flex flex-row-reverse space-x-reverse space-x-16 mb-32"> {/* Increased margin-bottom */}
+        <motion.div className="flex flex-row-reverse space-x-reverse space-x-6 lg:space-x-16 mb-20 lg:mb-32">
           {firstRow.map((product) => (
             <ProductCard
               product={product}
@@ -80,7 +80,7 @@ export const HeroParallax: React.FC<{ products: { title: string; link: string; t
             />
           ))}
         </motion.div>
-        <motion.div className="flex flex-row mb-32 space-x-16"> {/* Increased margin-bottom */}
+        <motion.div className="flex flex-row space-x-6 lg:space-x-16 mb-20 lg:mb-32">
           {secondRow.map((product) => (
             <ProductCard
               product={product}
@@ -89,7 +89,7 @@ export const HeroParallax: React.FC<{ products: { title: string; link: string; t
             />
           ))}
         </motion.div>
-        <motion.div className="flex flex-row-reverse space-x-reverse space-x-16"> {/* Adjusted margin */}
+        <motion.div className="flex flex-row-reverse space-x-reverse space-x-6 lg:space-x-16">
           {thirdRow.map((product) => (
             <ProductCard
               product={product}
@@ -99,34 +99,21 @@ export const HeroParallax: React.FC<{ products: { title: string; link: string; t
           ))}
         </motion.div>
       </motion.div>
-      {/* Gradient Transition Overlay */}
-      <div className="absolute bottom-0 left-0 w-full h-20 bg-gradient-to-t from-black to-transparent"></div> {/* Adjusted height */}
+      <div className="absolute bottom-0 left-0 w-full h-16 bg-gradient-to-t from-black to-transparent"></div>
 
-      {/* New Section with Header and Description */}
-      <div className="absolute bottom-0 w-full bg-black text-left z-40 pt-32 px-8"> {/* Set text alignment to left and added padding */}
-        <div className="max-w-4xl mx-auto flex items-start"> {/* Use flex for icon and text alignment */}
-          <HelpCircle className="text-white h-12 w-12 mr-4" /> {/* Increased icon size */}
-          <div> {/* Container for text */}
-            <h2 className="text-white text-2xl md:text-4xl font-bold mb-4">
-              Kaip atrodo puslapio kurimo darbo eiga?
-            </h2>
-            <p className="text-zinc-400 text-lg md:text-xl">
-              Puslapio kūrimas yra nuoseklus procesas, apimantis svarbius etapus nuo idėjų iki galutinio produkto. Kiekvienas etapas užtikrina, kad svetainė atitiktų jūsų lūkesčius ir poreikius.
-            </p>
+      <div className="absolute bottom-0 w-full bg-black text-left z-40 pt-16 lg:pt-32 px-4 lg:px-8">
           </div>
         </div>
-      </div>
-    </div>
   );
 };
 
 export const Header: React.FC = () => {
   return (
-    <div className="relative max-w-7xl mx-auto py-12 md:py-20 px-4 w-full left-0 top-0 z-40"> {/* Adjusted padding */}
-      <h1 className="text-2xl md:text-5xl font-bold text-white"> {/* Adjusted font size */}
+    <div className="relative max-w-7xl mx-auto py-12 md:py-20 px-4 w-full left-0 top-0 z-40">
+      <h1 className="text-2xl md:text-5xl font-bold text-white">
         Mūsų <span className="text-2xl md:text-5xl font-bold"><FlipWords words={["nuostabi", "motyvuota", "geriausia"]} /></span> <br /> Brandforge komanda
       </h1>
-      <p className="max-w-2xl text-base md:text-lg mt-4 text-white"> {/* Adjusted font size */}
+      <p className="max-w-2xl text-base md:text-lg mt-4 text-white">
         Kuria gražius ir funkcionalius puslapius, naudodama įvairias technologijas.
         Siekiame greito darbo, patrauklaus dizaino, praktiškumo ir kainų, nuo kurių neišsigąsta jūsų piniginė.
       </p>
@@ -144,10 +131,10 @@ export const ProductCard: React.FC<{ product: { title: string; link: string; thu
         x: translate,
       }}
       whileHover={{
-        y: -20,
+        y: -10,
       }}
       key={product.title}
-      className="group/product h-80 w-[28rem] relative flex-shrink-0 overflow-hidden" 
+      className="group/product h-60 lg:h-80 w-[20rem] lg:w-[28rem] relative overflow-hidden" // Responsive height and width
     >
       <Link
         href={product.link}
